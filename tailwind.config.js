f4574f3f1990001d34f0dda224e5a58fbca0f435
Nastasiia03 <abernatskao@gmail.com>
2023-09-15 20:01:40 +0300

    start of stylization

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/index.html', './src/index.js'],
+  content: ['./src/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '1272': '1272px',
+      }
+    },
     colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
       'background': '#FCF7E6',
+      black: colors.black,
+      white: colors.white,
     }
   },
   plugins: [],
